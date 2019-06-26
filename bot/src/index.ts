19c844e0a3ab1e7ffe5b307d669a6a2a0f64e85c
Abziz <Abziz89@gmail.com>
2019-06-26 22:27:44 +0300

    remove useless build steps and pass linting

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -12,8 +12,6 @@ function start() {
     const client = new Discord.Client();
     client.on('ready', async () => {
         console.log(`logged in as ${client.user.tag}`);
-        // const channels = client.channels.filter((channel) => channel.type === 'voice').map((ch) => ch as VoiceChannel);
-        // channels.forEach((ch) => console.log(`${ch.name}: ${ch.members.map((mem) => mem).join(",")}`));
     });
     client.login(process.env.DISCORD_TOKEN);
 }
