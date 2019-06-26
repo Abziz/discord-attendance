31f0c54911a53a5f40b92dec13902ac05d4ee7a7
Abziz <Abziz89@gmail.com>
2019-06-26 22:43:53 +0300

    change tslint config to airbnb one

--- bot/src/models/Log.ts
+++ bot/src/models/Log.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 const LogSchema = new Schema({
-   event: {type: String, index: true, required: true},
-   time: {type: Date, index: true, required: true}
+  event: { type: String, index: true, required: true },
+  time: { type: Date, index: true, required: true },
 });
 export const Log = mongoose.model('Log', LogSchema);
