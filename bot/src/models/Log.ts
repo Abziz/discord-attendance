2782e22b1bd6a4a80b28139377e7c72123e47a3b
Abziz <Abziz89@gmail.com>
2019-06-28 19:54:48 +0300

    add voice channel snapshoting logic

--- bot/src/models/Log.ts
+++
@@ -1,6 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-const LogSchema = new Schema({
-  event: { type: String, index: true, required: true },
-  time: { type: Date, index: true, required: true },
-});
-export const Log = mongoose.model('Log', LogSchema);
