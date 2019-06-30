9c1c90a1cd2f30b21110a79623c62529c36fce9e
Abziz <Abziz89@gmail.com>
2019-07-01 02:33:13 +0300

    Add counters to mongo db, bug fixes change snapshot logic

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,19 +1,9 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { AttendanceBot } from './AttendanceBot';
-
 dotenv.config();
 
-connect();
-
-function connect() {
-  mongoose.connection
-    .on('connect', () => { console.log('connected'); })
-    .on('error', console.error)
-    .once('open', start);
-  return mongoose.connect(process.env.MONGODB_URI!, { useNewUrlParser: true });
-}
-
-function start() {
+mongoose.connection.on('open', () => {
   new AttendanceBot(process.env.DISCORD_TOKEN!);
-}
+});
+mongoose.connect(process.env.MONGODB_URI!, { useNewUrlParser: true });
